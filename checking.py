5bfd0661f79d0e8ecbf095ea1b82630884bc97fa
Parsu2706 <sangaleprasad2706@gmail.com>
2025-12-21 02:05:15 +0530

    Remove checking.py

--- checking.py
+++
@@ -1,7 +0,0 @@
-from src.infer import load_pipeline, predict
-
-model, tok, l2i, i2l = load_pipeline()
-
-text = "The patient has chest pain, was started on aspirin, and scheduled for a CT scan."
-preds = predict(text, model, tok, i2l)
-print(preds)
